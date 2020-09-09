8749fcf3c0c6161c95fd4f04dcd89d75d9e06408
ankit <letscode1509@gmail.com>
2020-09-09 12:20:00 +0530

    Day-3

---
+++ Day-3/salary2.py
@@ -0,0 +1,27 @@
+salaries = ['$876,001', '$543,903', '$2453,896'] 
+
+
+# version 1
+
+new_salaries=[]
+
+for salary in salaries:
+    s = salary[1:]
+    s1 = s.split(',')
+    s2 = "".join(s1)
+    s3 = int(s2)
+    new_salaries.append(s3)
+    # Using the Chaining Concept
+    #new_salaries.append(int("".join(salary[1:].split(','))))
+print(new_salaries)    
+
+
+
+# version 2
+salaries = ['$876,001', '$543,903', '$2453,896'] 
+new_salaries=[]
+
+for salary in salaries:
+    new_salaries.append(int("".join(salary[1:].split(','))))
+
+print(new_salaries)    
